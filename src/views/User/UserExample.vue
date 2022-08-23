eca047c101d458aba2494b21a85144a42819fb6a
dhiaaziz <azizwkwkwk@gmail.com>
2022-08-23 16:33:42 +0700

    init + coba2 component

---
+++ src/views/User/UserExample.vue
@@ -0,0 +1,473 @@
+<script setup>
+// import TheWelcome from "@/components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="pisah-2">
+    <div class="tagihan child-pisah-2 border-success">
+      <div class="border-0 tagihan-child grandchild-pisah-2 card">
+        <div class="card-header"><h5>Tagihan</h5></div>
+        <div class="card-body data-tagihan">
+          <h5>paket</h5>
+          <h5>:</h5>
+          <h5 class="">paket 1 tahun</h5>
+          <p>description</p>
+          <p>:</p>
+          <p class="">diskon 10%</p>
+          <p>kamar</p>
+          <p>:</p>
+          <p class="">102</p>
+          <p>duration</p>
+          <p>:</p>
+          <p class="">20 ferbuari 2022 - 20 ferburari 2023</p>
+          <p>harga</p>
+          <p>:</p>
+          <p class="">770000 <span class="text-danger">- 500000</span></p>
+        </div>
+        <div class="m-1 progress">
+          <div
+            class="progress-bar bg-success"
+            role="progressbar"
+            aria-label="Success example"
+            style="width: 25%"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </div>
+    </div>
+    <div class="profile child-pisah-2 border-danger">
+      <div
+        class="profile-child grandchild-pisah-2 d-flex justify-content-between"
+      >
+        <div
+          class="mx-4 data-profile d-flex flex-column justify-content-center"
+        >
+          <h6>username</h6>
+          <p>mona</p>
+          <h6>email</h6>
+          <p>mona@gmail.com</p>
+          <h6>kamar</h6>
+          <p>ali 102 | 16 juni 2023</p>
+        </div>
+        <div class="">
+          <img
+            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+            class="h-100 img-fluid"
+            alt="..."
+          />
+        </div>
+      </div>
+    </div>
+    <div class="friend child-pisah-2 border-primary">
+      <div class="border-0 friend-child grandchild-pisah-2 card">
+        <div class="card-header"><h5>Friend</h5></div>
+        <div class="my-0 overflow-auto card-body">
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+          <div class="data-friend d-flex">
+            <div class="image">
+              <img
+                src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kOH7Wo7DrHXEC4z_lBOIdC8dXh50m0oRwGDduCPQ7eX1SQ6_"
+                class="w-100 img-fluid rounded-circle"
+                alt="..."
+              />
+            </div>
+            <div
+              class=" data-child-friend ms-3 d-flex flex-column justify-content-center"
+            >
+              <p>108 | 30 januari 2022 - 30 juni 2022</p>
+              <p>dani</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="message child-pisah-2 border-warning">
+      <div class="message-child grandchild-pisah-2">
+        <div class="px-2 py-0 overflow-auto chat">
+          <div class="chat-message d-flex">
+            <div class="m-1 d-flex flex-column">
+              <img
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                class="rounded-circle align-self-center justify-content-center"
+                alt="Chris Wood"
+                width="40"
+                height="40"
+              />
+              <div class="mt-2 text-muted small text-nowrap">2:41 am</div>
+            </div>
+            <div
+              class="px-3 py-2 mr-3 rounded  chat-message-text flex-shrink-1 bg-light"
+            >
+              <div class="mb-1 font-weight-bold">mona</div>
+              Morbi finibus, lorem id placerat ullamcorper, nunc enim ultrices
+              massa, id dignissim metus urna eget purus.
+            </div>
+          </div>
+          <div class="flex-row-reverse chat-message d-flex">
+            <div class="m-1 d-flex flex-column">
+              <img
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                class="mr-1  rounded-circle align-self-center justify-content-center"
+                alt="Chris Wood"
+                width="40"
+                height="40"
+              />
+              <div class="mt-2 text-muted small text-nowrap">2:41 am</div>
+            </div>
+            <div
+              class="px-3 py-2 mr-3 rounded  chat-message-text flex-shrink-1 bg-light text-end"
+            >
+              <div class="mb-1 font-weight-bold">mona</div>
+              Morbi finibus, lorem id placerat ullamcorper, nunc enim ultrices
+              massa, id dignissim metus urna eget purus.
+            </div>
+          </div>
+          <div class="chat-message d-flex">
+            <div class="m-1 d-flex flex-column">
+              <img
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                class="mr-1  rounded-circle align-self-center justify-content-center"
+                alt="Chris Wood"
+                width="40"
+                height="40"
+              />
+              <div class="mt-2 text-muted small text-nowrap">2:41 am</div>
+            </div>
+            <div
+              class="px-3 py-2 mr-3 rounded  chat-message-text flex-shrink-1 bg-light"
+            >
+              <div class="mb-1 font-weight-bold">mona</div>
+              Morbi finibus, lorem id placerat ullamcorper, nunc enim ultrices
+              massa, id dignissim metus urna eget purus.
+            </div>
+          </div>
+          <div class="flex-row-reverse chat-message d-flex">
+            <div class="m-1 d-flex flex-column">
+              <img
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                class="mr-1  rounded-circle align-self-center justify-content-center"
+                alt="Chris Wood"
+                width="40"
+                height="40"
+              />
+              <div class="mt-2 text-muted small text-nowrap">2:41 am</div>
+            </div>
+            <div
+              class="px-3 py-2 mr-3 rounded  chat-message-text flex-shrink-1 bg-light text-end"
+            >
+              <div class="mb-1 font-weight-bold">mona</div>
+              Morbi finibus, lorem id placerat ullamcorper, nunc enim ultrices
+              massa, id dignissim metus urna eget purus.
+            </div>
+          </div>
+          <div class="chat-message d-flex">
+            <div class="m-1 d-flex flex-column">
+              <img
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                class="mr-1  rounded-circle align-self-center justify-content-center"
+                alt="Chris Wood"
+                width="40"
+                height="40"
+              />
+              <div class="mt-2 text-muted small text-nowrap">2:41 am</div>
+            </div>
+            <div
+              class="px-3 py-2 mr-3 rounded  chat-message-text flex-shrink-1 bg-light"
+            >
+              <div class="mb-1 font-weight-bold">mona</div>
+              Morbi finibus, lorem id placerat ullamcorper, nunc enim ultrices
+              massa, id dignissim metus urna eget purus.
+            </div>
+          </div>
+          <div class="flex-row-reverse chat-message d-flex">
+            <div class="m-1 d-flex flex-column">
+              <img
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                class="mr-1  rounded-circle align-self-center justify-content-center"
+                alt="Chris Wood"
+                width="40"
+                height="40"
+              />
+              <div class="mt-2 text-muted small text-nowrap">2:41 am</div>
+            </div>
+            <div
+              class="px-3 py-2 mr-3 rounded  chat-message-text flex-shrink-1 bg-light text-end"
+            >
+              <div class="mb-1 font-weight-bold">mona</div>
+              Morbi finibus, lorem id placerat ullamcorper, nunc enim ultrices
+              massa, id dignissim metus urna eget purus.
+            </div>
+          </div>
+        </div>
+        <div class="form-chat">
+          <div
+            class="m-1  form-chat-child w-100 bg-warning d-flex align-self-center"
+          >
+            <input
+              type="text"
+              name=""
+              id=""
+              class="px-3 py-2 text-white w-100 bg-warning"
+              style="border: none"
+            />
+            <div class="align-items-center d-flex pe-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="currentColor"
+                class="bi bi-send align-items-center"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.pisah-2 {
+  /* display:flex;
+    flex-direction: row;
+    flex-flow: wrap; */
+  /* width: 100%;
+    height: 100vh;
+    margin: 0%;
+    padding:0px; */
+  /* background-color: aqua; */
+  height: 96vh;
+  width: 98vw;
+  display: grid;
+  margin: 2vh 1vw;
+  grid-template-columns: 6fr 5fr;
+  /* grid-template-rows: 10%; */
+  justify-items: stretch;
+  gap: 2vh;
+}
+.pisah-2 p,
+.pisah-2 h5 {
+  margin: 0px;
+  padding: 0px;
+}
+.child-pisah-2 {
+  /* background-color: yellowgreen; */
+  /* display: inline; */
+  border: solid 2px;
+  border-radius: 8px;
+}
+.grandchild-pisah-2 {
+  /* background-color: bisque; */
+  /* margin: 2vh 1vw; */
+}
+.child-pisah-2:nth-child(odd) {
+  /* float: left; */
+  /* width: 60%; */
+}
+.child-pisah-2:nth-child(even) {
+  /* float: right; */
+  /* width: 40%; */
+}
+.child-pisah-2:nth-child(odd) .grandchild-pisah-2 {
+  /* margin: 0.7rem 0rem 0rem 0.7rem; */
+}
+.child-pisah-2:nth-child(even) .grandchild-pisah-2 {
+  /* margin: 0.7rem 0.7rem 0rem 0.7rem; */
+}
+.child-pisah-2:last-child {
+  /* clear:right; */
+}
+
+.tagihan {
+  grid-row: 1/4;
+  height: 38vh;
+}
+.tagihan-child {
+  height: 100%;
+}
+.data-tagihan {
+  display: grid;
+  grid-template-columns: 10fr 1fr 30fr;
+}
+
+.friend {
+  grid-row: 4/10;
+  height: 56vh;
+}
+.friend-child {
+  height: 100%;
+}
+.data-friend {
+  height: 50px;
+  margin-bottom: 10px;
+}
+.data-friend .image {
+  width: 50px;
+  border: solid 1px blue;
+  border-radius: 100%;
+}
+.data-child-friend {
+}
+
+.profile {
+  grid-row: 1/3;
+  height: 28vh;
+}
+.profile-child {
+  height: 100%;
+}
+.profile-child img {
+  border-radius: 0px 6px 6px 0px;
+}
+.data-profile {
+  height: 28vh;
+}
+.data-profile h6 {
+  font-size: 0.8em;
+  margin: 0px;
+  padding: 0px;
+}
+.data-profile p {
+  font-size: 1.1em;
+  margin: 0px;
+  padding: 0px;
+  margin-bottom: 0.3em;
+}
+
+.message {
+  grid-row: 3/10;
+  height: 66vh;
+  position: relative;
+}
+.message-child {
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
+}
+.chat {
+  max-height: 58vh;
+}
+.chat-message {
+  margin: 0.3em 0em;
+}
+.chat-message-text {
+  width: 75%;
+}
+.form-chat {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.form-chat-child,
+.form-chat-child input {
+  border-radius: 8px;
+}
+
+/* @media (min-width: 1024px) {
+    body {
+      display: flex;
+      place-items: center;
+    }
+
+    #app {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      padding: 0 2rem;
+    }
+  } */
+</style>
